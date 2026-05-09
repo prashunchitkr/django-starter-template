364a6ed2c83c052cbb66d14dc33ef633b594d855
Prashun Chitrakar <prashunchi@gmail.com>
2026-05-09 15:37:42 +0545

    feat: add DRF throttling
    Add DEFAULT_THROTTLE_CLASSES and DEFAULT_THROTTLE_RATES for anonymous and authenticated users.

--- core/settings/_rest_framework.py
+++ core/settings/_rest_framework.py
@@ -8,5 +8,13 @@ class RestFrameworkSettings:
         ],
         "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "DEFAULT_THROTTLE_CLASSES": [
+            "rest_framework.throttling.AnonRateThrottle",
+            "rest_framework.throttling.UserRateThrottle",
+        ],
+        "DEFAULT_THROTTLE_RATES": {
+            "anon": "100/day",
+            "user": "1000/day",
+        },
         "PAGE_SIZE": 20,
     }
