2041e998ea9df9b6b54468cbe01046f01c868d45
Prashun Chitrakar <prashunchi@gmail.com>
2026-05-09 14:54:21 +0545

    refactor: reorder settings imports and MRO
    Alphabetize settings class imports and inheritance order for consistency. Add ValidatorsSettings.

--- core/settings/_base.py
+++ core/settings/_base.py
@@ -1,36 +1,35 @@
 from configurations import Configuration
 from dotenv import load_dotenv
 
-from core.settings._apps import AppsSettings
-from core.settings._cache import CacheSettings
-from core.settings._cors import CorsSettings
-from core.settings._database import DatabaseSettings
-from core.settings._i18n import I18NSettings
-from core.settings._jwt import JWTSettings
-from core.settings._middlewares import MiddlewareSettings
-from core.settings._rest_framework import RestFrameworkSettings
 from core.settings._security import SecuritySettings
+from core.settings._i18n import I18NSettings
+from core.settings._database import DatabaseSettings
+from core.settings._cache import CacheSettings
+from core.settings._rest_framework import RestFrameworkSettings
+from core.settings._jwt import JWTSettings
+from core.settings._cors import CorsSettings
+from core.settings._validators import ValidatorsSettings
 from core.settings._static import StaticFilesSettings
 from core.settings._templates import TemplateSettings
-from core.settings._validators import ValidatorsSettings
+from core.settings._middlewares import MiddlewareSettings
+from core.settings._apps import AppsSettings
 
 
 class BaseSettings(
-    JWTSettings,
-    RestFrameworkSettings,
-    TemplateSettings,
-    CacheSettings,
+    SecuritySettings,
+    I18NSettings,
     DatabaseSettings,
+    CacheSettings,
+    RestFrameworkSettings,
+    JWTSettings,
     CorsSettings,
     ValidatorsSettings,
     StaticFilesSettings,
-    SecuritySettings,
+    TemplateSettings,
     MiddlewareSettings,
     AppsSettings,
-    I18NSettings,
     Configuration,
 ):
-
     load_dotenv()
 
     @classmethod
